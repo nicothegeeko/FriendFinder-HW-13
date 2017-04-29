6d6132ba166bc7b3a843195ef06cc6817c36dca1
Nico Flicker <Nico@Nicos-MacBook-Air.local>
2017-04-28 17:52:57 -0700

    updating html elements and api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,8 +18,9 @@ module.exports = function(app) {
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
+  
 
-  app.get("/api/survey", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friendsArray);
   });
 
@@ -32,7 +33,7 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/survey", function(req, res) {
+  app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     if (friendsArray.length < 5) {
